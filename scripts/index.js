adff580094606a81513fe7b395f672baeb128607
theo shellman <theoshellman@gmail.com>
2025-01-08 19:13:57 -0500

    adding js for openig and closeing .modal

--- scripts/index.js
+++ scripts/index.js
@@ -10,4 +10,17 @@ const initalCard = [
   { name: "Mountain house", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg" },
 ];
 
+const profileEditButton = document.querySelector('.profile__edit-btn');
 
+const editProfileModal = document.querySelector('#edit-profile-modal');
+const closeProfilemodal = editProfileModal.querySelector('.modal__close-btn');
+
+function openModal() {
+  editProfileModal.classList.add('modal__opened');
+}
+  function closeModal() {
+    editProfileModal.classList.remove('modal__opened');
+
+}
+profileEditButton.addEventListener('click', openModal);
+closeProfilemodal.addEventListener('click', closeModal);

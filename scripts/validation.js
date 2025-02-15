b0594147148b6b2ebcb7f3c70f9049a245a695ce
theo shellman <theoshellman@gmail.com>
2025-02-14 20:12:13 -0500

    added ID to sapn tags for error msg

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,23 +1,26 @@
-const showInputError = (formEl, inputEl, errorMessage) => {
-    const errorElement = formEl.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.add("form__input_type_error");
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add("form__input-error_active");
+const showInputError = (formEl, inputEl, errorMsg) => {
+    const errorMsgID = inputEl.id + "-error";
+    const errorMsgEl = document.querySelector("#" + errorMsgID)
+    errorMsgEl.textContent = errorMsg
+    
+    // inputElement.classList.add("form__input_type_error");
+    // errorElement.textContent = errorMessage;
+    // errorElement.classList.add("form__input-error_active");
   };
   
-  const hideInputError = (formElement, inputElement) => {
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-    inputElement.classList.remove("form__input_type_error");
-    errorElement.classList.remove("form__input-error_active");
-    errorElement.textContent = "";
-  };
+//   const hideInputError = (formElement, inputElement) => {
+//     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+//     inputElement.classList.remove("form__input_type_error");
+//     errorElement.classList.remove("form__input-error_active");
+//     errorElement.textContent = "";
+//   };
 
 const checkInputValidity = (formEl, inputEl) => {
-    // if (!inputEl.validity.valid) {
-    //   showInputError(formEl, inputEl, inputEl.validationMessage);
+    if (!inputEl.validity.valid) {
+      showInputError(formEl, inputEl, inputEl.validationMessage);
     // } else {
     //   hideInputError(formEl, inputEl);
-    // }
+    }
   };
 
 const setEventListeners = (formEl) => {
@@ -26,7 +29,7 @@ const setEventListeners = (formEl) => {
   
     // toggleButtonState(inputList, buttonElement);
   
-    inputList.forEach((inputEl) => {
+    inputList.forEach((inputElement) => {
       inputElement.addEventListener("input", function () {
         checkInputValidity(formEl, inputElement);
         // toggleButtonState(inputList, buttonElement);
@@ -40,5 +43,4 @@ formList.forEach((formEl) => {
     setEventListeners(formEl);
 });
 };
-
-enableValiadation();
+enableValiadation()

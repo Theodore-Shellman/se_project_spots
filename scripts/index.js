8df10f8b0c3568d5f1af4376b9c042455f9ef31d
theo shellman <theoshellman@gmail.com>
2025-03-03 10:30:34 -0500

    add escapeKey

--- scripts/index.js
+++ scripts/index.js
@@ -89,12 +89,24 @@ function getCardElement(data) {
 previewModalClose.addEventListener("click", () => {
   closeModal(previewModal);
 });
+function escapeKey(){
+  
+  document.addEventListener('keyup', (event) => {
+    console.log("addin g event listener") 
+    if (event.key === 'Escape') {
+      const escapeModal = document.querySelector("modal_opened");
+      console.log(modal)
+      console.log(escapeModal)
+    }
+  });}
+function openModal(modal,) {
 
-function openModal(modal) {
   modal.classList.add("modal_opened");
+  
 }
 function closeModal(modal) {
-  modal.classList.remove("modal_opened");
+ modal.classList.remove("modal_opened");
+  
 }
 function handleEditFormSubmit(evt) {
   evt.preventDefault();
